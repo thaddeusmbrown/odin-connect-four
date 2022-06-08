f14149075c1d5889ec74bdb4682ddec4a6d96da1
Thaddeus Brown <thaddeusmbrown@gmail.com>
2022-06-07 21:33:41 -0500

    Add functionality for taking turns, remove redundant tests.  Need to learn more about testing rescue statements because the stub method oversimplified testing to the point of being non-value add.

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,31 +1,129 @@
 require 'pry-byebug'
 
 class ConnectFour
-  attr_reader :game_board, :player_list
+  attr_reader :game_board, :player_list, :player
 
   def initialize
-    @player_list = []
+    @game_board = Array.new(6) { Array.new(7, ' ') }
+    @player_list = ['X', 'O']
+    query_player()
   end
 
-  def new_game
+  protected
 
+  def query_player
+    @player = @player_list[0]
+    puts "\nPlayer #{@player}: Please choose a column from 1 to 7."
+    begin
+      answer = gets.chop.to_i - 1
+    rescue StandardError
+      puts "Sorry.  You chose an invalid character!  Choose a number from 1 to 7. Try again."
+      query_player()
+    else
+      check_column(answer)
+    end
   end
 
-  def place_tile(column, player)
+  def check_column(answer)
+    # binding.pry
+    begin
+      raise unless answer.between?(0, 6)
+    rescue StandardError
+      puts "Sorry.  You have to choose a column between 1 and 7!  Try again."
+      query_player()
+    end
+    check_row(answer)
+  end
 
+  def check_row(answer)
+    @game_board.each_with_index do |row, index|
+      if row[answer] == ' '
+        place_tile(index, answer)
+        if check_victory()
+          exit(true)
+        end
+        @player_list[0], @player_list[1] = @player_list[1], @player_list[0]
+        query_player()
+      end
+    end
+    puts "Sorry.  You have to choose a column that isn't full! Try again."
+    query_player()
+  end
+
+  def place_tile(row, column)
+    @game_board[row][column] = @player
+    display_board()
   end
 
   def check_victory
+    @game_board.each do |row|
+      counter = 0
+      player = ''
+      row.each do |cell|
+        if cell == ' '
+          counter = 0
+        elsif counter.zero?
+          counter = 1
+          player = cell
+        elsif cell == player
+          counter += 1
+          if counter == 4
+            victory(player)
+            return 1
+          end
+        else
+          player = ''
+          counter = 0
+        end
+      end
+    end
+    columns = @game_board.transpose
+    columns.each do |col|
+      counter = 0
+      player = ''
+      col.each do |cell|
+        if cell == ' '
+          counter = 0
+        elsif counter.zero?
+          counter = 1
+          player = cell
+        elsif cell == player
+          counter += 1
+          if counter == 4
+            victory(player)
+            return 1
+          end
+        else
+          player = ''
+          counter = 0
+        end
+      end
+    end
+    return false
+  end
+
+  def victory(player)
+    puts "Victory! Player #{player} wins!"
+    return 1
 
   end
+
+  def display_board()
+    top_row = ''
+    (1..7).each do |num|
+      top_row += '| ' + num.to_s + ' '
+    end
+    puts top_row += '|'
+    puts '-' * 29
+    @game_board.reverse.each do |row|
+      middle_row = ''
+      row.each do |col|
+        middle_row += "| #{col} "
+      end
+      middle_row += '|'
+      puts middle_row
+      puts '-' * 29
+    end
+  end
 end
-
-
-check_board = [
-  ['X', 0, 0, 0, 0, 0, 0],
-  [0, 'X', 0, 0, 0, 0, 0],
-  [0, 0, 'X', 0, 0, 0, 0],
-  [0, 0, 0, 'X', 0, 0, 0]
-] + Array.new(3, Array.new(7, 0))
-
-p check_board
+ConnectFour.new
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,57 +1,38 @@
 require './lib/connect_four'
 
 describe 'ConnectFour' do
-  describe 'game start and basic functions' do
-    before(:each) do
-      @main = ConnectFour.new
-      @main.stub(:new_game)
-      @main.stub(:place_tile).with(an_instance_of(Integer), an_instance_of(String))
-      @main.new_game
-    end
-    it 'creates an empty game board' do
-      check_board = Array.new(6) { Array.new(7, 0) }
-      game_board = @main.instance_variable_get(:@game_board)
-      expect(game_board).to eql(check_board)
-    end
+  before(:each) do
+    @main = ConnectFour.new
+    @main.stub(:query_player)
+    STDOUT.stub(:write)
+    @main.stub(:place_tile).with(an_instance_of(Integer), an_instance_of(Integer))
+  end
+  describe '#place_tile' do
     it 'places a tile of the correct player in a non-empty column' do
-      check_board = Array.new(6) { Array.new(7, 0) }
+      check_board = Array.new(6) { Array.new(7, ' ') }
+      @main.instance_variable_set(:@game_board, check_board)
       check_board[0][0] = 'X'
-      @main.place_tile(0, 'X')
+      @main.instance_variable_set(:@player, 'X')
+      @main.send(:place_tile, 0, 0)
       game_board = @main.instance_variable_get(:@game_board)
       expect(game_board).to eql(check_board)
-    end
-    it 'raises an exception when the player tries to place a piece outside the gameboard' do
-      @main.place_tile(-1, 'X')
-      expect { @main.place_tile(-1, 'X') }
-        .to raise_exception StandardError
-    end
-    it 'raises an exception when the player tries to place a piece in a full column' do
-      full_board = Array.new(6) { Array.new(7, 'X') }
-      expect { @main.place_tile(0, 'X') }
-        .to raise_exception StandardError
-    end
-    it 'changes the current player after each turn' do
-      @main.place_tile(0, 'X')
-      next_player = @main.player_list[0]
-      expect(next_player).to eql('O')
     end
   end
   describe 'checking for victory' do
     before(:each) do
       @main = ConnectFour.new
-      @main.stub(:check_victory)
     end
     it 'successfully finds a horizontal victory' do
-      check_board = check_board = [[0, 'X', 'X', 'X', 'X', 0, 0]] + Array.new(5) { Array.new(7, 0) }
-      result = @main.check_victory
-      expect(result).to eql(true)
+      @main.instance_variable_set(:@game_board, [[0, 'X', 'X', 'X', 'X', 0, 0]] + Array.new(5) { Array.new(7, 0) } )
+      result = @main.send(:check_victory)
+      expect(result).to eql(1)
     end
     it 'successfully finds a vertical victory' do
-      check_board = Array.new(4, ['X'] + Array.new(6, 0)) + Array.new(3, Array.new(7, 0))
-      result = @main.check_victory
-      expect(result).to eql(true)
+      @main.instance_variable_set(:@game_board, Array.new(4, ['X'] + Array.new(6, 0)) + Array.new(3, Array.new(7, 0)))
+      result = @main.send(:check_victory)
+      expect(result).to eql(1)
     end
-    it 'successfully finds a forward diagonal victory' do
+    xit 'successfully finds a forward diagonal victory' do
       check_board = [
         ['X', 0, 0, 0, 0, 0, 0],
         [0, 'X', 0, 0, 0, 0, 0],
@@ -59,9 +40,9 @@ describe 'ConnectFour' do
         [0, 0, 0, 'X', 0, 0, 0]
       ] + Array.new(3, Array.new(7, 0))
       result = @main.check_victory
-      expect(result).to eql(true)
+      expect(result).to eql(1)
     end
-    it 'successfully finds a backward diagonal victory' do
+    xit 'successfully finds a backward diagonal victory' do
       check_board = [
         [0, 0, 0, 'X', 0, 0, 0],
         [0, 0, 'X', 0, 0, 0, 0],
@@ -69,7 +50,7 @@ describe 'ConnectFour' do
         ['X', 0, 0, 0, 0, 0, 0]
       ] + Array.new(3, Array.new(7, 0))
       result = @main.check_victory
-      expect(result).to eql(true)
+      expect(result).to eql(1)
     end
   end
 end
